addd75f576d7b899c8cdcdcee19920868eb27062
per1234 <accounts@perglass.com>
2023-01-03 01:34:40 -0800

    Initial commit of script

--- README.md
+++ README.md
@@ -11,3 +11,12 @@
 [**gvm**](https://github.com/moovweb/gvm) is a version manager for the Go programming language. Unfortunately **gvm** does not support Windows.
 
 This script provides a partial emulation of the CLI of [**gvm**](https://github.com/moovweb/gvm) to allow the same commands to be used for management of Go version on all platforms.
+
+## Installation
+
+1. Add the path containing the script to the `PATH` environment variable.
+1. Add the following line to `~/.bashrc`:
+   ```text
+   alias gvm="source gvm.sh"
+   ```
+   **ⓘ** The `source` is a hack to allow the script to modify the environment variables of the shell the script is executed from, as is required for the `gvm use` invocation to work.
---
+++ gvm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+verb="$1"
+argument="$2"
+
+if [[ "$verb" == "" ]]; then
+  echo "error: Verb not provided."
+  exit 1
+fi
+
+if [[ "$verb" != "help" && "$argument" == "" ]]; then
+  echo "error: Go version not provided."
+  exit 1
+fi
+
+if [[ "$verb" == "help" ]]; then
+  echo "Usage: gvm <verb> [Go version]"
+  echo "Example: gvm use go1.17"
+  echo "Verbs:"
+  echo "- install: Install the specified Go version."
+  echo "- use: Use the specified Go version."
+  echo "- help: Display help."
+elif [[ "$verb" == "install" ]]; then
+  go install golang.org/dl/${argument}@latest
+  ${argument} download
+elif [[ "$verb" == "use" ]]; then
+  export GOROOT="$(${argument} env GOROOT)"
+  # The path must be converted to POSIX format before being added to the PATH environment variable.
+  posix_goroot="$(cygpath "${GOROOT}")"
+  export PATH="${posix_goroot}/bin:${PATH}"
+else
+  echo "error: Verb $verb unknown."
+  exit 1
+fi
